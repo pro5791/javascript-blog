7532d3784acfd8f4b38c165fc608f597731b126c
Przemek Rogala <przemek@rogala.priv.pl>
2019-10-17 20:47:03 +0200

    Add functionality /* add class 'active' to the clicked link */ js/script.js

--- js/script.js
+++ js/script.js
@@ -7,16 +7,22 @@ const titleClickHandler = function(event){
     console.log('Link was clicked!');
     console.log(event);
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
+    const clickedElement = this;
+        clickedElement.classList.add('active');
 
-    /* remove class 'active' from all articles */
+    console.log('clickedElement:', clickedElement);
+    //console.log('clickedElement (with plus): ' + clickedElement);
+
+
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .post.active');
 
     for(let activeArticle of activeArticles){
