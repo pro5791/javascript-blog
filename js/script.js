d1f75a96be42b47a7421f17fe41b7da2754bce33
Przemek Rogala <przemek@rogala.priv.pl>
2019-10-21 20:26:48 +0200

    Add new data from task (generateTitleLinks)

--- js/script.js
+++ js/script.js
@@ -45,7 +45,34 @@ const titleClickHandler = function(event) {
 
     /* [DONE] add class 'active' to the correct article */
     targetArticle.classList.add('active');
+
 }
+
+/* task 6.4 */
+
+    const optArticleSelector = '.post',
+          optTitleSelector = '.post-title',
+          optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+    /* remove contents of titleList */
+
+    /* for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
 
 const links = document.querySelectorAll('.titles a');
 
