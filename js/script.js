7d3cb86d1815b1f4e53d3744c5daf3ba540cc292
Przemek Rogala <przemek@rogala.priv.pl>
2019-11-03 18:07:20 +0100

    Add code code to script.js - /* [DONE] make a new constant tag and extract tag from the href constant */

--- js/script.js
+++ js/script.js
@@ -151,7 +151,9 @@ function tagClickHandler(event){
     const href = clickedElement.getAttribute("href");
     //console.log('href:', href);
 
-    /* make a new constant "tag" and extract tag from the "href" constant */
+    /* [DONE] make a new constant "tag" and extract tag from the "href" constant */
+    const tag = href.replace('#tag-', '');
+    //console.log('tag:', tag);
 
     /* find all tag links with class active */
 
